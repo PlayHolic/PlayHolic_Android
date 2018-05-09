ac6f2fc12895b250e797e4c15e7f69ac1d33b1f6
Jiyunn <jygr0309@gmail.com>
2018-05-09 21:09:15 +0900

    init commit

---
+++ app/src/main/java/me/plic/playholic_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.plic.playholic_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
