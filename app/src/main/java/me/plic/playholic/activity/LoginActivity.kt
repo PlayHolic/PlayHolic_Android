a99861bf3cd00eefd0731bfda8c0b8304f727beb
Jiyunn <jygr0309@gmail.com>
2018-07-16 20:39:32 +0900

    #20-Splah 화면 생성 및 기능 추가

---
+++ app/src/main/java/me/plic/playholic/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package me.plic.playholic.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.plic.playholic.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
