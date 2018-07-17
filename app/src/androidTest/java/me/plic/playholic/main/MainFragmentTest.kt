dd5c83af2e1cd1d529d2fe69da32ee2c5e01970a
Jiyunn <jygr0309@gmail.com>
2018-07-17 15:22:25 +0900

    #38-(Test)

---
+++ app/src/androidTest/java/me/plic/playholic/main/MainFragmentTest.kt
@@ -0,0 +1,32 @@
+package me.plic.playholic.main
+
+import android.support.test.rule.ActivityTestRule
+import me.plic.playholic.R
+import me.plic.playholic.activity.MainActivity
+import me.plic.playholic.fragment.HistoryFragment
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class MainFragmentTest {
+
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun onCreate() {
+
+    }
+
+    @Test
+    fun applyFragment() {
+        activity.activity?.apply {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_main, HistoryFragment())
+                    .addToBackStack(null)
+                    .commit()
+        }
+    }
+}
---
+++ app/src/androidTest/java/me/plic/playholic/main/MainFragmentViewModelTest.kt
@@ -0,0 +1,37 @@
+package me.plic.playholic.main
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import me.plic.playholic.R
+import me.plic.playholic.activity.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
+class MainFragmentViewModelTest {
+
+    private val switchScreen = MainFragmentTest()
+
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule(MainActivity::class.java)
+
+
+    @Test
+    fun historyButton_ShowsScreen() {
+        onView(withId(R.id.btn_history)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun historyButton_PerformClick() {
+        onView(withId(R.id.btn_history)).perform(click())
+        applyFragment()
+    }
+
+    private fun applyFragment() {
+        switchScreen.applyFragment()
+    }
+}
