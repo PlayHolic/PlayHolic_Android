ddf5fadcd494cd2ed7c89b633290c431ed98fd34
Jiyunn <jygr0309@gmail.com>
2018-07-18 15:04:17 +0900

    #61-마이페이지 프래그먼트 클래스 추가

---
+++ app/src/main/java/me/plic/playholic/fragment/MyPageFragment.kt
@@ -0,0 +1,29 @@
+package me.plic.playholic.fragment
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.plic.playholic.R
+import me.plic.playholic.databinding.FragmentMypageBinding
+
+class MyPageFragment : Fragment() {
+
+    lateinit var binding : FragmentMypageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mypage, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
