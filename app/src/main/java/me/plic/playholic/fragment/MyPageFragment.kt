134adb46573ef0352360083d57c455507d23d281
Jiyunn <jygr0309@gmail.com>
2018-07-18 16:50:08 +0900

    #66-마이페이지에 툴바 적용

--- app/src/main/java/me/plic/playholic/fragment/MyPageFragment.kt
+++ app/src/main/java/me/plic/playholic/fragment/MyPageFragment.kt
@@ -4,9 +4,11 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import me.plic.playholic.R
+import me.plic.playholic.activity.MainActivity
 import me.plic.playholic.databinding.FragmentMypageBinding
 
 class MyPageFragment : Fragment() {
@@ -25,5 +27,26 @@ class MyPageFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        (activity as MainActivity).apply {
+            setSupportActionBar(this@MyPageFragment.binding.toolbar?.toolbar)
+
+            supportActionBar?.apply {
+                title = getString(R.string.menu_my)
+                setDisplayHomeAsUpEnabled(true)
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            activity?.onBackPressed()
+            return true
+        }
+        return false
     }
 }
