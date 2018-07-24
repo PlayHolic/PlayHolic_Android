1ba8ad4100c2273ef3766f0bae4e9ded7ae04ca1
Jiyunn <jygr0309@gmail.com>
2018-07-24 16:53:55 +0900

    #117-메인 -> 위시리스트 이동 기능 추가

--- app/src/main/java/me/plic/playholic/ui/main/MainFragmentViewModel.kt
+++ app/src/main/java/me/plic/playholic/ui/main/MainFragmentViewModel.kt
@@ -1,14 +1,36 @@
 package me.plic.playholic.ui.main
 
 import android.arch.lifecycle.ViewModel
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.view.View
+import me.plic.playholic.R
 import me.plic.playholic.common.SwitchScreen
+import me.plic.playholic.ui.wish.WishFragment
 
 class MainFragmentViewModel : ViewModel() {
 
     lateinit var switchScreen: SwitchScreen
+    var fgManager: FragmentManager? = null
 
     fun applyFragment() {
         if (::switchScreen.isInitialized) switchScreen.applyFragment()
         else throw UninitializedPropertyAccessException("switchScreen is not initialized")
+    }
+
+    fun onFABClick(view: View) {
+        when (view.id) {
+            R.id.fab_wish -> applyFragment(WishFragment())
+        }
+
+    }
+
+    private fun applyFragment(fragment: Fragment) {
+        fgManager?.apply {
+            beginTransaction()
+                    .replace(R.id.frame_main, WishFragment())
+                    .addToBackStack(null)
+                    .commit()
+        }
     }
 }
