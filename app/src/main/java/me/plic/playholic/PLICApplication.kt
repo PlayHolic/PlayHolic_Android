5e2f1d307b614d8d50032ece5eba1d06c28c7447
Jiyunn <jygr0309@gmail.com>
2018-07-31 14:16:37 +0900

    #183-Realm initialize

--- app/src/main/java/me/plic/playholic/PLICApplication.kt
+++ app/src/main/java/me/plic/playholic/PLICApplication.kt
@@ -1,10 +1,12 @@
 package me.plic.playholic
 
 import android.app.Application
+import io.realm.Realm
 
 class PLICApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Realm.init(this)
     }
 }
