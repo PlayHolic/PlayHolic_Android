e68797fecc42366d753b37e53fd94a9176fe0d02
Jiyunn <jygr0309@gmail.com>
2018-07-31 12:50:26 +0900

    #174-ViewPager 적용

---
+++ app/src/main/java/me/plic/playholic/common/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,32 @@
+package me.plic.playholic.common.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import me.plic.playholic.ui.search.PopKeywordFragment
+import me.plic.playholic.ui.search.RecentKeywordFragment
+
+
+class ViewPagerAdapter(fm: FragmentManager,
+                       val pageCount: Int,
+                       val tabTitles: Array<String>) : FragmentStatePagerAdapter(fm) {
+
+
+    override fun getItem(position: Int): Fragment {
+        when (position) {
+            0 -> RecentKeywordFragment()
+            1 -> PopKeywordFragment()
+        }
+        return RecentKeywordFragment()
+    }
+
+
+    override fun getCount(): Int {
+        return pageCount
+    }
+
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return tabTitles[position]
+    }
+}
--- app/src/main/java/me/plic/playholic/ui/search/SearchActivity.kt
+++ app/src/main/java/me/plic/playholic/ui/search/SearchActivity.kt
@@ -1,17 +1,37 @@
 package me.plic.playholic.ui.search
 
-import android.app.Activity
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_search.view.*
 import me.plic.playholic.R
+import me.plic.playholic.common.adapter.ViewPagerAdapter
 import me.plic.playholic.databinding.ActivitySearchBinding
 
-class SearchActivity : Activity() {
+class SearchActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivitySearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
+
+        initToolbar()
+        initViewPager()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbar.toolbar).apply {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    private fun initViewPager() {
+        binding.vpSearch.apply {
+            adapter = ViewPagerAdapter(supportFragmentManager, 2,
+                    arrayOf(getString(R.string.recent_keyword), getString(R.string.pop_keyword)))
+
+            binding.tabSearch.setupWithViewPager(this)
+        }
     }
 }
