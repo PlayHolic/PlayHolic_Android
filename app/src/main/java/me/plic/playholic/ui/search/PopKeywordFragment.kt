e331d35a06c898356a078c9a200c6b6f1142b9db
Jiyunn <jygr0309@gmail.com>
2018-08-01 13:38:24 +0900

    #201-PopKeyword 리스트 기능 추가

--- app/src/main/java/me/plic/playholic/ui/search/PopKeywordFragment.kt
+++ app/src/main/java/me/plic/playholic/ui/search/PopKeywordFragment.kt
@@ -7,20 +7,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import me.plic.playholic.R
+import me.plic.playholic.data.PopKeyword
 import me.plic.playholic.databinding.LayoutKeywordBinding
+import me.plic.playholic.ui.search.adapter.PopKeywordAdapter
 
 class PopKeywordFragment : Fragment() {
 
-    private lateinit var  binding : LayoutKeywordBinding
+    private lateinit var binding: LayoutKeywordBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.layout_keyword, container, false)
-
         return binding.root
     }
 
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerKeyword.apply {
+            adapter = PopKeywordAdapter().apply {
+                for (i in 1..10) {
+                    addItem(PopKeyword(i, "검색어 $i"))
+                }
+            }
+        }
     }
 }
