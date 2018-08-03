f9a91107393244227546b5d52f7e09f5c42e936e
Jiyunn <jygr0309@gmail.com>
2018-08-03 10:03:10 +0900

    #228-AutoKeyword adapter 추가

---
+++ app/src/main/java/me/plic/playholic/ui/search/adapter/AutoKeywordAdapter.kt
@@ -0,0 +1,80 @@
+package me.plic.playholic.ui.search.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.Filterable
+import me.plic.playholic.data.AutoKeyword
+import me.plic.playholic.ui.search.viewHolder.AutoKeywordViewHolder
+
+class AutoKeywordAdapter(context: Context?,
+                         val resource: Int,
+                         val itemList: MutableList<AutoKeyword>) : ArrayAdapter<AutoKeyword>(context, resource), Filterable {
+
+
+    private var suggestions: MutableList<AutoKeyword> = mutableListOf() //auto complete keyword
+    private var inputWord: String = "" //input Word
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+
+        if (convertView == null) {
+            view = LayoutInflater.from(parent?.context).inflate(resource, parent, false)
+
+            AutoKeywordViewHolder(view).apply {
+                view.tag = this
+            }
+
+        } else {
+            convertView.tag as AutoKeywordViewHolder
+            view = convertView
+        }
+        return view
+    }
+
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): Filter.FilterResults {
+
+                if (constraint != null) {
+                    suggestions.clear()
+                    inputWord = constraint.toString()
+
+                    for (auto in itemList) {
+                        if (auto.keyword.contains(inputWord)) {
+                            suggestions.add(auto)
+                        }
+                    }
+
+                    return FilterResults().apply {
+                        values = suggestions
+                        count = suggestions.size
+                    }
+                }
+
+                return FilterResults()
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: Filter.FilterResults?) {
+                if (results != null && results.count > 0) {
+
+                    this@AutoKeywordAdapter.clear()
+
+                    (results.values as MutableList<*>?)?.also {
+                        for (auto in it) {
+                            if (auto is AutoKeyword) {
+                                add(auto)
+                            }
+                        }
+                        notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/me/plic/playholic/ui/search/viewHolder/AutoKeywordViewHolder.kt
@@ -0,0 +1,13 @@
+package me.plic.playholic.ui.search.viewHolder
+
+import android.databinding.DataBindingUtil
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import me.plic.playholic.databinding.ItemKeywordAutoBinding
+
+class AutoKeywordViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    val binding : ItemKeywordAutoBinding? = DataBindingUtil.bind(itemView)
+
+
+}
