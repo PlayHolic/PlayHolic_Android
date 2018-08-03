95a50f77e85a43016cd30c17fcfba484cf648501
Jiyunn <jygr0309@gmail.com>
2018-08-03 10:08:01 +0900

    #221-검색 중 기능 구현

--- app/src/main/java/me/plic/playholic/common/binding/KeywordBinding.kt
+++ app/src/main/java/me/plic/playholic/common/binding/KeywordBinding.kt
@@ -1,11 +1,35 @@
 package me.plic.playholic.common.binding
 
 import android.databinding.BindingAdapter
+import android.graphics.Typeface
+import android.support.v4.content.ContextCompat
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.widget.TextView
+import me.plic.playholic.R
 import java.text.SimpleDateFormat
 import java.util.*
 
 @BindingAdapter("searchDay")
 fun dateToDayMonth(view: TextView, date: Date) {
     view.text = SimpleDateFormat("MM.dd", Locale.KOREAN).format(date)
+}
+
+
+@BindingAdapter("inputWord", "keyword")
+fun changeColorInputWord(view: TextView, inputWord: String, keyword: String) {
+    val wordIndex = keyword.indexOf(inputWord)
+
+    if (wordIndex >= 0) {
+        SpannableString(keyword).apply {
+            setSpan(ForegroundColorSpan(ContextCompat.getColor(view.context, R.color.colorPrimary)),
+                    wordIndex, wordIndex + inputWord.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            setSpan(StyleSpan(Typeface.BOLD), wordIndex, wordIndex + inputWord.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            view.text = this
+        }
+    }
 }
